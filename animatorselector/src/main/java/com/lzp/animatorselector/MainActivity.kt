0fe643780780b15b97412e311daeb060ebb22ec0
lizhipeng <lizhipeng@ugirls.com>
2019-04-08 14:48:22 +0800

    新增学习案例animated-selector

--- animatorselector/src/main/java/com/lzp/animatorselector/MainActivity.kt
+++ animatorselector/src/main/java/com/lzp/animatorselector/MainActivity.kt
@@ -2,11 +2,26 @@ package com.lzp.animatorselector
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val STATE_CHECKED = intArrayOf(android.R.attr.state_checked)
+    private val STATE_UNCHECKED = intArrayOf()
+    private var flag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image.setOnClickListener {
+            if (flag) {
+                image.setImageState(STATE_UNCHECKED, true)
+                flag = false
+            } else {
+                image.setImageState(STATE_CHECKED, true)
+                flag = true
+            }
+        }
     }
 }
